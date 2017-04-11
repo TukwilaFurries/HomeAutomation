66b243cd7fbec620d331f9c16147a4c60b8689fe
Kyle Morrison <cerb@thetridog.com>
2017-04-11 03:19:44 -0700

    Removing more .pyc

--- import1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-class Import1A:
-    def begin(self):
-        print "Begin Import1A"
-
-class Import1B:
-    def begin(self):
-        print "Begin Import1B"
--- import2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-class Import2A:
-    def begin(self):
-        print "Begin Import2A"
-
-class Import2B:
-    def begin(self):
-        print "Begin Import2B"
